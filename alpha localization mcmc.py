c20aaae194111c0094bbfad41dfae9ab57683472
Chulin Wang <31944527+PureHyd@users.noreply.github.com>
2019-07-14 16:30:41 -0500

    Fixed hyperparameters

--- alpha localization mcmc.py
+++ alpha localization mcmc.py
@@ -11,10 +11,13 @@ from scipy.special import digamma
 from scipy import optimize
 import datetime
 
+np.random.seed(1874617301)
+
 def deltaG(params, B):
     B_d, B_dphi, A = params
-    B_d = np.abs(B_d)
-    B_dphi = np.abs(B_dphi)
+    A = A + 1e-20
+    B_d = np.abs(B_d) + 1e-20
+    B_dphi = np.abs(B_dphi) + 1e-20
     return(A * (digamma(1/2 + B_dphi/(2*B)) - digamma(1/2 + B_d/(2*B)) + np.log(B_d/B_dphi)))
     
 def Cost(params, X_data, Y_data):
@@ -42,7 +45,7 @@ hbar = 1.0545718e-34
 prefct = 4*np.pi**2 * hbar / ee**2
 
 X = B_1
-Y = G_1 * prefct
+Y = G_1
 
 leng = X.shape[0]
 
@@ -52,15 +55,15 @@ data = []
 
 t = datetime.datetime.now()
 print(t)
-Ntry = 1000
+Ntry = 100
 for i in range(Ntry):
     N = leng // 20
-    idx = np.random.randint(0,leng, size=N)
+    idx = np.random.randint(0, leng, size=N)
     Y_data = Y[idx]
     X_data = np.abs(X[idx])
     initialParam = [np.random.exponential(scale=16), np.random.exponential(scale=16),\
-                    np.random.exponential(scale=100)]
-    Niter = int(1e5)
+                    np.random.exponential(scale=10)]
+    Niter = int(5e4)
     dist = 1
     T = 1e8
     param = initialParam
@@ -70,7 +73,7 @@ for i in range(Ntry):
     cost_all = []
     cost = Cost(param, X_data, Y_data)
     decay = np.power(1/T, 1/Niter)
-    T = 10
+    T = 1
     for a, step in zip(acc, steps):
         p = np.abs(param * (1 + step*dist))
         if(Cost(p, X_data, Y_data) / Cost(param, X_data, Y_data)) < np.exp(a * T):
@@ -80,12 +83,12 @@ for i in range(Ntry):
         cost_all.append(cost)
         T = T * decay
     tnow = datetime.datetime.now()
-    eta = (Ntry-i-1) / (i+1) * (tnow - t) + t
+    eta = (Ntry-i-1) / (i+1) * (tnow - t) + tnow
     print("{}\t{}\t{}".format(eta, i, cost))
     data.append((X_data, Y_data))
     paramstore.append(param)
     resid.append(cost)
-paramstore = np.array(initialParam)
+paramstore = np.array(paramstore)
 resid = np.array(resid)
 
 plt.figure()
@@ -98,13 +101,19 @@ plt.plot(np.abs(X)[order], Y[order], "r")
 resultLen = resid.shape[0]
 resultOrd = np.argsort(resid)
 nn = 4 #  top 1/4
-sortedParam = paramstore[resultOrd[:resultLen//nn]]
+
 sortedCost = resid[resultOrd[:resultLen//nn]]
+sortedParam = []
+for param in paramstore[resultOrd[:resultLen//nn]]:
+    param = np.abs(param)
+    sortedParam.append([max(param[:2]), min(param[:2]), param[2] * prefct])
 
 for param, cost in zip(sortedParam, sortedCost):
     plt.plot(xx, deltaG(param, xx))
-    param = np.abs(param)
-    print("N:  {:.8f}\tB_d:  {:.8e}\tB_dphi:  {:.8e}\tcost:  {:.8e}"\
-          .format(max(param[:2]), min(param[:2]), param[2], cost))
+    print("B_d:  {:.8f}\tB_dphi:  {:.8f}\tN:  {:.8f}\tcost:  {:.8e}"\
+          .format(param[0], param[1], param[2], cost))
 plt.legend(["Experiment","Localization fit"])
 
+finalParam = np.mean(sortedParam, axis=0)
+print("Final:\nB_d:  {:.8f}\tB_dphi:  {:.8f}\tN:  {:.8f}"\
+      .format(finalParam[0], finalParam[1], finalParam[2]))
